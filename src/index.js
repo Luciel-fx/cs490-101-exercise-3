9c6705e82b5e426585607ca61686a22f954ba4e2
Ivan Zhu <ivanzhu201@gmail.com>
2025-09-28 19:42:56 -0400

    Implemented a microservice that allows users to be created, retrieved, updated, and deleted. User data is stored in memory (no database).

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const crypto = require('node:crypto');
 const app = express();
 const port = 3000;
 
@@ -9,8 +10,67 @@ app.use(express.json());
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const users = [];
+
+// CREATE a user
+app.post('/users', (req, res) => {
+    const { name, email } = req.body || {};
+
+    if (!name || !email) {
+        return res.sendStatus(400);
+    }
+
+    const user = { 
+        id: crypto.randomUUID(), name, email 
+    };
+
+    users.push(user);
+    return res.status(201).json(user);
+});
+
+// RETRIEVE a user
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = users.find(u => u.id === id);
+
+    if (!user) {
+        return res.sendStatus(404);
+    }
+
+    return res.status(200).json(user);
+});
+
+// UPDATE a user
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body || {};
+    const i = users.findIndex(u => u.id === id);
+
+    if (i === -1) {
+        return res.sendStatus(404);
+    }
+    if (!name || !email) {
+        return res.sendStatus(400);
+    }
+
+    users[i].name = name;
+    users[i].email = email;
+
+    return res.status(200).json(users[i]);
+});
+
+// DELETE a user
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const i = users.findIndex(u => u.id === id);
+
+    if (i === -1) {
+        return res.sendStatus(404);
+    }
+
+    users.splice(i, 1);
+
+    return res.sendStatus(204);
 });
 
 // Do not touch the code below this comment
